f1bfbde8f0f794ae8c65848b40ec41105f8c9314
[YanaB] <[tudabae@gmail.com]>
2023-07-08 15:19:59 +0300

    количество четных чисел в массиве

---
+++ zd1_count_chet/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] arr = GetArray();
+PrintArray(arr);
+Console.Write($"Количество четных чисел -> {GetCountEvenNumbers(arr)}");
+
+
+int[] GetArray(){
+    int[] array = new int[20];
+    for(int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+ void PrintArray(int[] array)
+            {
+                Console.Write(String.Join(", ", array));
+                Console.WriteLine();
+                Console.Write(" ");
+            }
+int GetCountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i] % 2 == 0){
+            count++;
+        }
+    }return count;
+}
